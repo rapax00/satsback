53ef7ce3c2cedcbda9a1d4bf9e07ecd5260bc1b8
Rapax <gasparpelayo44@gmail.com>
2024-10-25 14:42:59 -0300

    fix: dont whitelist

--- src/utils/makeEvent.ts
+++ src/utils/makeEvent.ts
@@ -11,19 +11,19 @@ import { Volunteer } from '@prisma/client';
 
 dotenv.config();
 
-const whitelistPublicKeys = {
-    pulpo: '9c38f29d508ffdcbe6571a7cf56c963a5805b5d5f41180b19273f840281b3d45',
-    agustin: 'a1b0f9fdc84a81e1fa3b0289de83486792d68407f9e13baf22850f1f1f9b61b2',
-    juan: '3699cf3fab1aa22dad84155639d35911013c63bbe6e26818e2584ed12cebeb6e',
-    rapax: '994d52a31d3efd0ac661b1940e3f3dcae49c750d6dd90c68600589f272e3bc85',
-    dios: 'cee287bb0990a8ecbd1dee7ee7f938200908a5c8aa804b3bdeaed88effb55547',
-    el7: 'ff64654a88bee78dba2d1d9999b4224fb72d4821e32e722ae3871f438431021b',
-};
+// const whitelistPublicKeys = {
+//     pulpo: '9c38f29d508ffdcbe6571a7cf56c963a5805b5d5f41180b19273f840281b3d45',
+//     agustin: 'a1b0f9fdc84a81e1fa3b0289de83486792d68407f9e13baf22850f1f1f9b61b2',
+//     juan: '3699cf3fab1aa22dad84155639d35911013c63bbe6e26818e2584ed12cebeb6e',
+//     rapax: '994d52a31d3efd0ac661b1940e3f3dcae49c750d6dd90c68600589f272e3bc85',
+//     dios: 'cee287bb0990a8ecbd1dee7ee7f938200908a5c8aa804b3bdeaed88effb55547',
+//     el7: 'ff64654a88bee78dba2d1d9999b4224fb72d4821e32e722ae3871f438431021b',
+// };
 
-const whitelistVolunteers = {
-    pulpochorizo:
-        '308e83914a7d1b52b5497906f821b83ee0d4a417a50572d36a6c30169a3e968a',
-};
+// const whitelistVolunteers = {
+//     pulpochorizo:
+//         '308e83914a7d1b52b5497906f821b83ee0d4a417a50572d36a6c30169a3e968a',
+// };
 
 async function makeEvent(
     eTag: string,
@@ -34,12 +34,12 @@ async function makeEvent(
 ): Promise<NostrEvent> {
     try {
         // Check if user is allowed to make satsback // debug
-        if (
-            !Object.values(whitelistPublicKeys).includes(userPubkey) &&
-            !Object.values(whitelistVolunteers).includes(userPubkey)
-        ) {
-            throw new Error('User not allowed to make satsback');
-        }
+        // if (
+        //     !Object.values(whitelistPublicKeys).includes(userPubkey) &&
+        //     !Object.values(whitelistVolunteers).includes(userPubkey)
+        // ) {
+        //     throw new Error('User not allowed to make satsback');
+        // }
 
         // Calculate satsback amount
         let satsbackAmount: number;
